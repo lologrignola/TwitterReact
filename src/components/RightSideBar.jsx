d37f0693d4b7e39bf67f4304f0a98c30e0bdc5e0
FelipeCabreraB <felipemartincabrera@gmail.com>
2021-11-02 11:15:27 -0300

    Welcome

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -2,136 +2,147 @@ import React from "react";
 
 function RightSideBar() {
   return (
-  <>
-  
-  <div style="background-color: black" className="sticky-top pb-3">
-  <div className="pt-3">
-    <form action="/search-one" method="POST">
-      <input
-        type="text"
-        className="form-control rounded-pill"
-        name="username"
-        placeholder="Buscar en Twitter"
-        aria-label="Username"
-        aria-describedby="basic-addon1"
-      />
-      <input type="submit" style="display: none" />
-    </form>
-  </div>
-</div>
+    <>
+      <div style="background-color: black" className="sticky-top pb-3">
+        <div className="pt-3">
+          <form action="/search-one" method="POST">
+            <input
+              type="text"
+              className="form-control rounded-pill"
+              name="username"
+              placeholder="Buscar en Twitter"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+            <input type="submit" style="display: none" />
+          </form>
+        </div>
+      </div>
 
-<div className="tendencias rounded">
-  <div className="inside-tendencias">
-    <div className="row">
-      <div className="col-10">Tendencias para ti</div>
-      <div className="col-2">
-        <a className="tendencias-a functionallityNotWorking" href="#"><i className="fas fa-cog"></i></a>
-      </div>
-    </div>
-    <a className="tendencias-a functionallityNotWorking" href="#"
-      ><div className="tendencias-hover mt-2 p-2">
-        <div className="row tt-uru">
-          <div className="col-10">Tendencia en Uruguay</div>
-          <div className="col-2"><i className="fas fa-ellipsis-h"></i></div>
-        </div>
-        <p className="tendencias-a">Tabarez</p>
-        <p className="left-nav-text">34,7K Tweets</p>
-      </div>
-    </a>
-    <a className="tendencias-a functionallityNotWorking" href="#"
-      ><div className="tendencias-hover mt-2 p-2">
-        <div className="row tt-uru">
-          <div className="col-10">Tendencia en Uruguay</div>
-          <div className="col-2"><i className="fas fa-ellipsis-h"></i></div>
-        </div>
-        <p className="tendencias-a">AUF</p>
-        <p className="left-nav-text">31,6K Tweets</p>
-      </div>
-    </a>
-    <a className="tendencias-a functionallityNotWorking" href="#"
-      ><div className="tendencias-hover mt-2 p-2">
-        <div className="row tt-uru">
-          <div className="col-10">Tendencia en Uruguay</div>
-          <div className="col-2"><i className="fas fa-ellipsis-h"></i></div>
-        </div>
-        <p className="tendencias-a">DT de Uruguay</p>
-        <p className="left-nav-text">26,3K Tweets</p>
-      </div>
-    </a>
-    <a className="tendencias-a functionallityNotWorking" href="#"
-      ><div className="tendencias-hover mt-2 p-2">
-        <div className="row tt-uru">
-          <div className="col-10">Tendencia en Uruguay</div>
-          <div className="col-2"><i className="fas fa-ellipsis-h"></i></div>
-        </div>
-        <p className="tendencias-a">Diego Aguirre</p>
-        <p className="left-nav-text">21,9K Tweets</p>
-      </div>
-    </a>
-    <a className="tendencias-a functionallityNotWorking" href="#"
-      ><div className="tendencias-hover mt-2 p-2">
-        <div className="row tt-uru">
-          <div className="col-10">Tendencia en Uruguay</div>
-          <div className="col-2"><i className="fas fa-ellipsis-h"></i></div>
-        </div>
-        <p className="tendencias-a">Complejo Celeste</p>
-        <p className="left-nav-text">15,2K Tweets</p>
-      </div>
-    </a>
-    <a href="" className="links-leftSide functionallityNotWorking"
-      ><p className="showMore mt-4">Mostrar más</p></a
-    >
-  </div>
-</div>
-<div id="stickyTopDiv">
-  <div className="mt-3 tendencias rounded">
-    <div className="inside-tendencias">
-      <div className="d-flex justify-content-between">
-        <div className="mt-2">A quién seguir</div>
-        <form action="/search" method="GET">
-          <button
-            type="submit"
-            className="align-self-end btn rounded-pill btn-twittear me-3 mb-2"
-            data-bs-toggle="modal"
-            data-bs-target="#searchToFollowModal"
-          >
-            Buscador
-          </button>
-        </form>
-      </div>
-      <hr />
-     {/*  <% for(user of randomUsers) { %> */}
-      <div className="mt-3">
-        <a className="tendencias-a" href="">
-          <div className="row g-0 justify-content-between tendencias-hover p-2">
-            <div className="col-3 d-lg-none d-xl-block">
-              <span className="TweetAuthor-avatar">
-                <div className="Avatar d-inline-block">
-                  <img
-                    className="rounded-circle"
-                    width="55rem"
-                    src="<%=user.avatar %>"
-                    alt="testUserAvatar"
-                  />
+      <div className="tendencias rounded">
+        <div className="inside-tendencias">
+          <div className="row">
+            <div className="col-10">Tendencias para ti</div>
+            <div className="col-2">
+              <a className="tendencias-a functionallityNotWorking" href="#">
+                <i className="fas fa-cog"></i>
+              </a>
+            </div>
+          </div>
+          <a className="tendencias-a functionallityNotWorking" href="#">
+            <div className="tendencias-hover mt-2 p-2">
+              <div className="row tt-uru">
+                <div className="col-10">Tendencia en Uruguay</div>
+                <div className="col-2">
+                  <i className="fas fa-ellipsis-h"></i>
                 </div>
-              </span>
+              </div>
+              <p className="tendencias-a">Tabarez</p>
+              <p className="left-nav-text">34,7K Tweets</p>
             </div>
-            <div className="col-6 d-flex flex-column">
-              <p>{/* <%=user.fullname %> */}</p>
-              <p className="userName">{/* @<%=user.username %> */}</p>
+          </a>
+          <a className="tendencias-a functionallityNotWorking" href="#">
+            <div className="tendencias-hover mt-2 p-2">
+              <div className="row tt-uru">
+                <div className="col-10">Tendencia en Uruguay</div>
+                <div className="col-2">
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+              </div>
+              <p className="tendencias-a">AUF</p>
+              <p className="left-nav-text">31,6K Tweets</p>
             </div>
-            <div className="col-6 col-xl-3 align-self-center">
-            {/*  IF ELSE CON LOS FORMS DE ABAJO CON ( ownUserFollowings.includes(user.id ) ADENTRO  */}
-              <form
-                id="btn-seguir-siguiendo"
-                className="align-self-center d-flex justify-content-end"
-                action="/unFollow/<%-user._id%>"
-                method="POST"
-              >
+          </a>
+          <a className="tendencias-a functionallityNotWorking" href="#">
+            <div className="tendencias-hover mt-2 p-2">
+              <div className="row tt-uru">
+                <div className="col-10">Tendencia en Uruguay</div>
+                <div className="col-2">
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+              </div>
+              <p className="tendencias-a">DT de Uruguay</p>
+              <p className="left-nav-text">26,3K Tweets</p>
+            </div>
+          </a>
+          <a className="tendencias-a functionallityNotWorking" href="#">
+            <div className="tendencias-hover mt-2 p-2">
+              <div className="row tt-uru">
+                <div className="col-10">Tendencia en Uruguay</div>
+                <div className="col-2">
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+              </div>
+              <p className="tendencias-a">Diego Aguirre</p>
+              <p className="left-nav-text">21,9K Tweets</p>
+            </div>
+          </a>
+          <a className="tendencias-a functionallityNotWorking" href="#">
+            <div className="tendencias-hover mt-2 p-2">
+              <div className="row tt-uru">
+                <div className="col-10">Tendencia en Uruguay</div>
+                <div className="col-2">
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+              </div>
+              <p className="tendencias-a">Complejo Celeste</p>
+              <p className="left-nav-text">15,2K Tweets</p>
+            </div>
+          </a>
+          <a href="" className="links-leftSide functionallityNotWorking">
+            <p className="showMore mt-4">Mostrar más</p>
+          </a>
+        </div>
+      </div>
+      <div id="stickyTopDiv">
+        <div className="mt-3 tendencias rounded">
+          <div className="inside-tendencias">
+            <div className="d-flex justify-content-between">
+              <div className="mt-2">A quién seguir</div>
+              <form action="/search" method="GET">
                 <button
-                  style="background-color: white; color: black; width: 90px"
                   type="submit"
-                  className="
+                  className="align-self-end btn rounded-pill btn-twittear me-3 mb-2"
+                  data-bs-toggle="modal"
+                  data-bs-target="#searchToFollowModal"
+                >
+                  Buscador
+                </button>
+              </form>
+            </div>
+            <hr />
+            {/*  <% for(user of randomUsers) { %> */}
+            <div className="mt-3">
+              <a className="tendencias-a" href="">
+                <div className="row g-0 justify-content-between tendencias-hover p-2">
+                  <div className="col-3 d-lg-none d-xl-block">
+                    <span className="TweetAuthor-avatar">
+                      <div className="Avatar d-inline-block">
+                        <img
+                          className="rounded-circle"
+                          width="55rem"
+                          src="<%=user.avatar %>"
+                          alt="testUserAvatar"
+                        />
+                      </div>
+                    </span>
+                  </div>
+                  <div className="col-6 d-flex flex-column">
+                    <p>{/* <%=user.fullname %> */}</p>
+                    <p className="userName">{/* @<%=user.username %> */}</p>
+                  </div>
+                  <div className="col-6 col-xl-3 align-self-center">
+                    {/*  IF ELSE CON LOS FORMS DE ABAJO CON ( ownUserFollowings.includes(user.id ) ADENTRO  */}
+                    <form
+                      id="btn-seguir-siguiendo"
+                      className="align-self-center d-flex justify-content-end"
+                      action="/unFollow/<%-user._id%>"
+                      method="POST"
+                    >
+                      <button
+                        style="background-color: white; color: black; width: 90px"
+                        type="submit"
+                        className="
                     align-self-center
                     btn
                     rounded-pill
@@ -140,12 +151,12 @@ function RightSideBar() {
                     mb-2
                     justify-content-end
                   "
-                >
-                  Siguiendo
-                </button>
-              </form>
+                      >
+                        Siguiendo
+                      </button>
+                    </form>
 
-              {/* <form
+                    {/* <form
                 id="btn-seguir-siguiendo"
                 className="align-self-center d-flex justify-content-end"
                 action="/follow/<%-user._id%>"
@@ -159,46 +170,52 @@ function RightSideBar() {
                   Seguir
                 </button>
               </form> */}
-
+                  </div>
+                </div>
+              </a>
             </div>
           </div>
-        </a>
-      </div>
+        </div>
 
-    </div>
-  </div>
-
-  <div className="under-navbar mt-4">
-    <div className="row">
-      <div className="col">
-        <a className="a-footer-welcome functionallityNotWorking" href="#">Condiciones de Servicio</a>
+        <div className="under-navbar mt-4">
+          <div className="row">
+            <div className="col">
+              <a className="a-footer-welcome functionallityNotWorking" href="#">
+                Condiciones de Servicio
+              </a>
+            </div>
+            <div className="col">
+              <a className="a-footer-welcome functionallityNotWorking" href="#">
+                Politica de Privacidad
+              </a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <a className="a-footer-welcome functionallityNotWorking" href="#">
+                Politica de Cookies
+              </a>
+            </div>
+            <div className="col">
+              <a className="a-footer-welcome functionallityNotWorking" href="#">
+                Info de anuncios
+              </a>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col mb-3">
+              <a className="a-footer-welcome functionallityNotWorking" href="#">
+                Mas opciones
+              </a>
+            </div>
+            <div className="col mb-3">
+              <p id="a-footer-welcome functionallityNotWorking">Twitter, Inc.</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col">
-        <a className="a-footer-welcome functionallityNotWorking" href="#">Politica de Privacidad</a>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col">
-        <a className="a-footer-welcome functionallityNotWorking" href="#">Politica de Cookies</a>
-      </div>
-      <div className="col">
-        <a className="a-footer-welcome functionallityNotWorking" href="#">Info de anuncios</a>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col mb-3">
-        <a className="a-footer-welcome functionallityNotWorking" href="#">Mas opciones</a>
-      </div>
-      <div className="col mb-3">
-        <p id="a-footer-welcome functionallityNotWorking">
-          Twitter, Inc.
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
-  </>
-);
+    </>
+  );
 }
 
 export default RightSideBar;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,7 +1,139 @@
 import React from "react";
 
 function Welcome() {
-  return <div></div>;
+  return (
+    <div style={{ backgroundColor: "white" }}>
+      <div className="row vw-100">
+        <div id="welcome-img" class="col">
+          <img src="/img/welcome-picture.png" alt="" />
+        </div>
+        <div id="happening-now-col" class="col">
+          <div className="py-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              className="bi bi-twitter"
+              viewBox="0 0 16 16"
+              id="welcome-logo"
+            >
+              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+            </svg>
+          </div>
+          <div>
+            <h1 className="fw-bolder pt-3" id="happening-now">
+              Happening now
+            </h1>
+          </div>
+          <div>
+            <h3 className="fw-bolder pt-4 pb-3" id="join-today">
+              Join Twitter today.
+            </h3>
+          </div>
+          <div>
+            <div className="d-flex flex-column">
+              <button className="sign-up-buttons functionallityNotWorking">
+                <i className="fab fa-google"></i>Sign up with Google
+              </button>
+              <button className="sign-up-buttons functionallityNotWorking">
+                <i className="fab fa-apple"></i>Sign up with Apple
+              </button>
+              <button
+                className="sign-up-buttons"
+                data-bs-toggle="modal"
+                data-bs-target="#registerModal"
+              >
+                Sign up with phone or email
+              </button>
+            </div>
+            <div className="mt-4">
+              <p className="text-muted" id="text-bellow-signup-buttons">
+                By signing up, you agree to the
+                <a className="sign-up-a-hover functionallityNotWorking" href="#">
+                  Terms of Service
+                </a>{" "}
+                and
+                <a className="sign-up-a-hover functionallityNotWorking" href="#">
+                  Privacy <br />
+                  Policy
+                </a>
+                , including
+                <a className="sign-up-a-hover functionallityNotWorking" href="#">
+                  Cookie Use.
+                </a>
+              </p>
+              <p className="mt-4" id="already-have-an-account">
+                Already have an account?
+                <a
+                  className="sign-up-a-nohover"
+                  href="#"
+                  data-bs-toggle="modal"
+                  data-bs-target="#loginModal"
+                >
+                  Sign in
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr id="welcome-hr" />
+      <footer className="d-flex justify-content-evenly align-items-center flex-wrap px-5 py-2">
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          About
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Help Center
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Terms of service
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Privacy Policy
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          {" "}
+          Cookie Policy
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Ads info
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Blog
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Status
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Careers
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Brand Resources
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Advertising
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Marketing
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Twitter for Business
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Developers
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Directory
+        </a>
+        <a className="functionallityNotWorking a-footer-welcome mx-1" href="#">
+          Settings
+        </a>
+        <p id="twitter-inc">&copy;</p>
+      </footer>
+      <script src="js/registerValidation.js"></script>
+    </div>
+  );
 }
 
 export default Welcome;
