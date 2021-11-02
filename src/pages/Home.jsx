844adcfbda5ff7b45046dbc80b19561138b92d1b
lologrignola <grignolabusiness@gmail.com>
2021-11-02 10:47:26 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,33 +4,33 @@ import RightSideBar from "../components/RightSideBar";
 
 function Home() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-2">
+    <div className="container">
+      <div className="row">
+        <div className="col-2">
           <LeftSideBar />
         </div>
-        <div class="col-10 col-lg-6">
+        <div className="col-10 col-lg-6">
           {/* NAVBAR ACA ***********************************************************************  */}
           <form action="/tweetear" method="POST">
-            <div class="top-input-tweet">
-              <div class="d-flex p-2" style="height: 100%">
-                <span class="TweetAuthor-avatar">
-                  <div class="Avatar ms-2 mt-2">
+            <div className="top-input-tweet">
+              <div className="d-flex p-2" style="height: 100%">
+                <span className="TweetAuthor-avatar">
+                  <div className="Avatar ms-2 mt-2">
                     <img
-                      class="rounded-circle"
+                      className="rounded-circle"
                       width="55rem"
                       src="<%= userAvatar %>"
                       alt="testUserAvatar"
                     />
                   </div>
                 </span>
-                <div class="d-flex flex-column" style="width: 100%">
-                  <div class="ps-3 mt-4" style="width: 100%">
+                <div className="d-flex flex-column" style="width: 100%">
+                  <div className="ps-3 mt-4" style="width: 100%">
                     <input
                       style="color: white"
                       type="text"
                       name="tweetText"
-                      class="form-control input-tweet-up inputNoborder"
+                      className="form-control input-tweet-up inputNoborder"
                       placeholder="Qué estas pensando?"
                       maxlength="140"
                       aria-label="Recipient's username"
@@ -39,7 +39,7 @@ function Home() {
                   </div>
                   <button
                     type="submit"
-                    class="align-self-end btn rounded-pill btn-twittear me-2 mb-2 mt-2"
+                    className="align-self-end btn rounded-pill btn-twittear me-2 mb-2 mt-2"
                   >
                     Tweettear
                   </button>
@@ -50,7 +50,7 @@ function Home() {
           <div id="tweetsContainer"></div>
           <input type="hidden" value="<%= userId %>" id="userIdDiv" />
         </div>
-        <div id="rightSidebarContainer" class="col-4">
+        <div id="rightSidebarContainer" className="col-4">
           <RightSideBar />
         </div>
       </div>
