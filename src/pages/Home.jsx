544898d849f24318600ca035a87b4452c49d1054
Lucas Gonzalez <lucas.carrau@hotmail.com>
2021-11-04 18:51:39 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,14 +42,11 @@ function Home() {
   const [dataLength, setdataLength] = useState(0);
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          `${process.env.REACT_APP_URL_BACKEND}/tweets/${page}`,{
-            headers: { "Content-Type": "application/json", Authorization: "Bearer " + token }
-          },
-        );
+        const response = await axios.get(`${process.env.REACT_APP_URL_BACKEND}/tweets/${page}`, {
+          headers: { "Content-Type": "application/json", Authorization: "Bearer " + token },
+        });
         console.log("DATA");
         console.log(response);
         setdataLength(dataLength + response.data.length);
@@ -62,69 +59,71 @@ function Home() {
   }, [page]);
 
   return (
-    <InfiniteScroll
-      dataLength={dataLength}
-      next={()=>setPage((prev) => prev + 1)}
-      hasMore={true}
-    >
     <div className="container">
       <div className="row">
         <div className="col-2">
           <LeftSideBar />
         </div>
+
         <div className="col-10 col-lg-6">
-          <TopNavbar text="Home" />
-          <form action="/tweetear" onSubmit={(ev) => handleSubmit(ev)} method="POST">
-            <div className="top-input-tweet">
-              <div className="d-flex p-2" style={{ height: "100%" }}>
-                <span className="TweetAuthor-avatar">
-                  <div className="Avatar ms-2 mt-2">
-                    <img
-                      className="rounded-circle"
-                      width="55rem"
-                      height="55rem"
-                      src="https://i.pinimg.com/originals/0d/36/e7/0d36e7a476b06333d9fe9960572b66b9.jpg"
-                      alt="testUserAvatar"
-                    />
+          <InfiniteScroll
+            dataLength={dataLength}
+            next={() => setPage((prev) => prev + 1)}
+            hasMore={true}
+          >
+            <TopNavbar text="Home" />
+            <form action="/tweetear" onSubmit={(ev) => handleSubmit(ev)} method="POST">
+              <div className="top-input-tweet">
+                <div className="d-flex p-2" style={{ height: "100%" }}>
+                  <span className="TweetAuthor-avatar">
+                    <div className="Avatar ms-2 mt-2">
+                      <img
+                        className="rounded-circle"
+                        width="55rem"
+                        height="55rem"
+                        src="https://i.pinimg.com/originals/0d/36/e7/0d36e7a476b06333d9fe9960572b66b9.jpg"
+                        alt="testUserAvatar"
+                      />
+                    </div>
+                  </span>
+                  <div className="d-flex flex-column" style={{ width: "100%" }}>
+                    <div className="ps-3 mt-4" style={{ width: "100%" }}>
+                      <input
+                        style={{ color: "white" }}
+                        type="text"
+                        name="tweetText"
+                        className="form-control input-tweet-up inputNoborder"
+                        placeholder="What's happening?"
+                        maxlength="140"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={tweetContent}
+                        onChange={(ev) => setTweetContent(ev.target.value)}
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="align-self-end btn rounded-pill btn-twittear me-2 mb-2 mt-2"
+                    >
+                      Tweet
+                    </button>
                   </div>
-                </span>
-                <div className="d-flex flex-column" style={{ width: "100%" }}>
-                  <div className="ps-3 mt-4" style={{ width: "100%" }}>
-                    <input
-                      style={{ color: "white" }}
-                      type="text"
-                      name="tweetText"
-                      className="form-control input-tweet-up inputNoborder"
-                      placeholder="What's happening?"
-                      maxlength="140"
-                      aria-label="Recipient's username"
-                      aria-describedby="button-addon2"
-                      value={tweetContent}
-                      onChange={(ev) => setTweetContent(ev.target.value)}
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="align-self-end btn rounded-pill btn-twittear me-2 mb-2 mt-2"
-                  >
-                    Tweet
-                  </button>
                 </div>
               </div>
+            </form>
+            <div id="tweetsContainer">
+              {tweets.map((tweet) => (
+                <Tweet tweet={tweet} />
+              ))}
             </div>
-          </form>
-          <div id="tweetsContainer">
-            {tweets.map((tweet) => (
-              <Tweet tweet={tweet} />
-            ))}
-          </div>
+          </InfiniteScroll>
         </div>
+
         <div id="rightSidebarContainer" className="col-4">
           <RightSideBar />
         </div>
       </div>
     </div>
-    </InfiniteScroll>
   );
 }
 
