62dc27dfd7ceb2c82d62d9aed8f9755aeb360825
FelipeCabreraB <felipemartincabrera@gmail.com>
2021-11-05 02:18:39 -0300

    popover

--- src/components/SideButtons.jsx
+++ src/components/SideButtons.jsx
@@ -1,20 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 
 function SideButtons({ icon, text, link, working }) {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header>
+        {" "}
+        <h6 className="text-black">Sorry! &#128546;</h6>{" "}
+      </Popover.Header>
+      <Popover.Body>This functionallity was not required for this proyect.</Popover.Body>
+    </Popover>
+  );
   return (
-    <li className="nav-item mt-2">
-      <Link to={link} style={{ textDecoration: "inherit" }}>
-        <div role="button" className={`links-leftSide ${working}`}>
-          <div className="row">
-            <div className="col-12 col-lg-2">
-              <i className={icon}></i>
+    <>
+      {working === "functionallityNotWorking" ? (
+        <OverlayTrigger trigger="click" rootClose placement="right" overlay={popover}>
+          <li className="nav-item mt-2">
+            <Link to={link} style={{ textDecoration: "inherit" }}>
+              <div role="button" className={`links-leftSide ${working}`}>
+                <div className="row">
+                  <div className="col-12 col-lg-2">
+                    <i className={icon}></i>
+                  </div>
+                  <div className="col-10 collapsable-sidebarleft">{text}</div>
+                </div>
+              </div>
+            </Link>
+          </li>
+        </OverlayTrigger>
+      ) : (
+        <li className="nav-item mt-2">
+          <Link to={link} style={{ textDecoration: "inherit" }}>
+            <div role="button" className={`links-leftSide ${working}`}>
+              <div className="row">
+                <div className="col-12 col-lg-2">
+                  <i className={icon}></i>
+                </div>
+                <div className="col-10 collapsable-sidebarleft">{text}</div>
+              </div>
             </div>
-            <div className="col-10 collapsable-sidebarleft">{text}</div>
-          </div>
-        </div>
-      </Link>
-    </li>
+          </Link>
+        </li>
+      )}
+    </>
   );
 }
 
